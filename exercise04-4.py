03676b702c2b89152eca50d4fc744fcd0cc8dfd6
david seol <hardroad07@gmail.com>
2022-05-18 17:49:37 +0900

    feature:exercise04-4 - print의 특성

---
+++ exercise04-4.py
@@ -0,0 +1,10 @@
+
+# 다음 중 출력 결과가 다른 것 한개를 골라 보자.
+
+#1 print('you' 'need' 'python')
+#2 print('you' + 'need' + 'python')
+#3 print('you', 'need', 'python')
+#4 print("".join(['you', 'need', 'python']))
+
+#답은 3번이다. print는 , 를 띄어쓰기로 취급하기때문에
+

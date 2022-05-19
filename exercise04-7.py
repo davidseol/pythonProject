97c57030fef4ab1188a5feaf791eaeee9f2ffedc
david seol <hardroad07@gmail.com>
2022-05-19 15:10:27 +0900

    feature:exercise04-7 - 파일 열고 닫고 , replace함수 이용해서 문자열 바꾸기

---
+++ exercise04-7.py
@@ -0,0 +1,15 @@
+
+#다음과 같은 내용을 지난 파일 test.txt가 있다. 이 파일의 내용 중 'java'라는 문자열을 'python'으로 바꾸어서 저장하기
+
+# life is too short
+# you need java
+
+f = open('test.txt', 'r')
+body = f.read()
+f.close()
+
+body = body.replace('java', 'python')
+
+f = open('test.txt', 'w')
+f.write(body)
+f.close()

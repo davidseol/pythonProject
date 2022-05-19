a39086412d8eec636d65458d66bad6ec45113842
david seol <hardroad07@gmail.com>
2022-05-19 17:29:22 +0900

    feature:exercise05-2 - class함수에서 생성자 __init__ 설정하기

---
+++ exercise05-2.py
@@ -0,0 +1,14 @@
+
+class fourcal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+
+a = fourcal(4, 2)
+print(a.add())
